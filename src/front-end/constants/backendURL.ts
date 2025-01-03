f2239248b0c15bafd09eb249773fb644dcc4c280
Y.C.Neo <elephantneo01@gmail.com>
2025-01-04 04:44:34 +0800

    ??

--- src/front-end/constants/backendURL.ts
+++ src/front-end/constants/backendURL.ts
@@ -1,7 +1,7 @@
 // const serverIP = "https://3.26.197.122";
 const domain = "https://groupup.neoycn.xyz";
 const expressLocation = "express";
-const socketLocation = "socket";
+const socketLocation = "socket.io";
 
 export const expressServer = `${domain}/${expressLocation}`;
 export const socketServer = `${domain}/${socketLocation}`;
