a7e9d73fdfd710741e85189f6e2b448f0f1539fe
Y.C.Neo <elephantneo01@gmail.com>
2025-01-04 02:29:25 +0800

    fix: deploy-client.sh

--- script/deploy-client.sh
+++ script/deploy-client.sh
@@ -1,4 +1,16 @@
+# check path
 cd ~/Cloud-Native-Final/src/front-end/
+echo "pwd: $(pwd)"
 
+# ensure the latest version of the code
+echo "git pull"
+git pull
+
+# install dependencies and start the server
+echo "npm install"
+npm install
+clear
+
+# start the server
 pm2 delete groupup-client
 pm2 start "npx expo start -w --non-interactive" --name groupup-client
